25fa2080754ce2ab668056d58052dbed6aaaec5e
AAteddy <tedsaasfaha@gmail.com>
2024-04-26 10:41:17 -0700

    [Error fixed] eslint raised issues solved.

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,16 +1,16 @@
 export default class Car {
-    constructor(brand, motor, color) {
-        this._brand = brand;
-        this._motor = motor; 
-        this._color = color;
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-    cloneCar() {
-        const cloned = new this.constructor();
-        const symbols = Object.getOwnPropertySymbols(this);
-        symbols.forEach((symbol) => {
-            cloned[symbol] = this[symbol];
-        });
-        return cloned;
-    }
+  cloneCar() {
+    const cloned = new this.constructor();
+    const symbols = Object.getOwnPropertySymbols(this);
+    symbols.forEach((symbol) => {
+      cloned[symbol] = this[symbol];
+    });
+    return cloned;
+  }
 }
