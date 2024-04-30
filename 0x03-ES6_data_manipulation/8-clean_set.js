51247231f510a8f468da50726fd88cd6b3e230d9
AAteddy <tedsaasfaha@gmail.com>
2024-04-30 08:55:07 -0700

    [Error fixed] eslint raised issues solved

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
-    if ( !set || !startString || !(set instanceof Set) || typeof(startString) !== 'string') {
-        return '';
-    }
-    return [...set]
+  if (!set || !startString || !(set instanceof Set) || typeof (startString) !== 'string') {
+    return '';
+  }
+  return [...set]
     .filter((element) => element && element.startsWith(startString))
     .map((str) => str.replace(startString, ''))
     .join('-');
