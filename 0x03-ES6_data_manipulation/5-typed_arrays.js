c8c04f4d449682ea92ff66f3c0d6338a4055fe3c
Tedros Alem <tedsaasfaha@gmail.com>
2024-04-30 16:49:08 +0300

    [Typed Arrays] project-6 file implemented

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,8 @@
+export default function createInt8TypedArray(length, position, value) {
+    if (position >= length) {
+        throw new Error('Position outside range');
+    }
+    const valInt8Array = new Int8Array(length);
+    valInt8Array.fill(value, position, position + 1);
+    return new DataView(valInt8Array.buffer);
+}
