f8ec06c50ed67e1526bffd5237fa5e19368f715f
Tedros Alem <tedsaasfaha@gmail.com>
2024-04-30 17:53:05 +0300

    [Clean set] project-9 file implemented.

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+    if ( !set || !startString || !(set instanceof Set) || typeof(startString) !== 'string') {
+        return '';
+    }
+    return [...set]
+    .filter((element) => element && element.startsWith(startString))
+    .map((str) => str.replace(startString, ''))
+    .join('-');
+}
