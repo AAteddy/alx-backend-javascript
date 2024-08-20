38f80a31cecd0e69cd710758d67ec3be0daa3a8b
Tedros <tediousalie@gmail.com>
2024-08-20 06:43:16 +0000

    1-stdin.js file updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
-	const input_data = process.stdin.read();
-	if (input_data !== null) {
-		process.stdout.write(`Your name is: ${input_data}`);
-	}
+  const input_data = process.stdin.read();
+  if (input_data !== null) {
+    process.stdout.write(`Your name is: ${input_data}`);
+  }
 });
 
 process.stdin.on('end', () => {
-	console.log('This important software is now closing');
+  console.log('This important software is now closing');
 });
