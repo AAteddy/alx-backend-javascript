734158910d8d3714157de57742ac66a6a91a443f
Tedros <tediousalie@gmail.com>
2024-08-20 10:51:44 +0000

    update 4-http.js file

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const app = http.createServer((request, response) => {
-  response.write("Hello Holberton School!");
+  response.write('Hello Holberton School!');
   response.end();
 }).listen(1245);
 
