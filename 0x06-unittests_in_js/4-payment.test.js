116a0189bf562abe7ae6f015df5446d3c7674552
Tedros <tediousalie@gmail.com>
2024-08-23 11:29:09 +0000

    [Sinon Stub] implementation of Sinon Stub & spies test suite

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,27 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  it('should stub Utils.calculateNumber and spy on console.log', function() {
+    // Create a stub for Utils.calculateNumber
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+    // Spy on console.log
+    const consoleSpy = sinon.spy(console, 'log');
+
+    // Call the function under test
+    sendPaymentRequestToApi(100, 20);
+
+    // Assert that the stub was called with the correct arguments
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+    // Assert that console.log was called with the correct message
+    expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+
+    // Restore the original functions after the test
+    calculateNumberStub.restore();
+    consoleSpy.restore();
+  });
+});
